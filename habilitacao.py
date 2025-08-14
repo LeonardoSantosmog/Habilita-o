dc09e23b5b2e47f66c6a62cf80e384c44dc819fb
Leonardo Santos <137180866+LeonardoSantosmog@users.noreply.github.com>
2025-08-14 13:45:41 -0300

    Add files via upload

---
+++ habilitacao.py
@@ -0,0 +1,7 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18:
+    print(f"Com {idade} anos, você pode se habilitação!!!")
+else:
+    print(f"Com {idade} anos, você não pode se habilitação")
